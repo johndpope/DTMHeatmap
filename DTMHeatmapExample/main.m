d1b6a3296089116a59e016a7961e7335baae931a
Bryan Oltman <bryanoltman@gmail.com>
2015-04-15 13:06:38 -0400

    Update copyright

--- DTMHeatmapExample/main.m
+++ DTMHeatmapExample/main.m
@@ -3,7 +3,7 @@
 //  DTMHeatMapExample
 //
 //  Created by Bryan Oltman on 1/7/15.
-//  Copyright (c) 2015 Bryan Oltman. All rights reserved.
+//  Copyright (c) 2015 Dataminr. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- Heatmaps/DTMHeatmap.m
+++ Heatmaps/DTMHeatmap.m
@@ -3,7 +3,7 @@
 //  HeatMapTest
 //
 //  Created by Bryan Oltman on 1/6/15.
-//  Copyright (c) 2015 Bryan Oltman. All rights reserved.
+//  Copyright (c) 2015 Dataminr. All rights reserved.
 //
 
 #import "DTMHeatmap.h"
